48f05965bf81703ed908a4eb960227cd3b8ff7fa
Alexandr <yaborodinov@gmail.com>
2021-07-05 10:56:42 +0300

    добавил футер, подключил навигацию на кнопки меню, реализовал переключение индивидуальных и групповых занятий, по мелочи добавил адаптив

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,2 +1,29 @@
 
-console.log('file 1');
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", (e) => {
+  pageYOffset > 101 ? header.classList.add("header--dark") : header.classList.remove("header--dark");
+  
+})
+
+
+const bntGroupLesson = document.getElementById("group-lessons-btn");
+const bntIndividualLesson = document.getElementById("individual-lessons-btn");
+
+const blockGroupLesson = document.getElementById("group-lessons-block")
+const blockIndividualLesson = document.getElementById("individual-lessons-block")
+
+bntGroupLesson.addEventListener("click",() => {
+  bntGroupLesson.classList.remove("btn__submit--deactivated");
+  bntIndividualLesson.classList.add("btn__submit--deactivated")
+  blockGroupLesson.classList.add("price__block--active");
+  blockIndividualLesson.classList.remove("price__block--active");
+})
+
+
+bntIndividualLesson.addEventListener("click", () => {
+  bntGroupLesson.classList.add("btn__submit--deactivated");
+  bntIndividualLesson.classList.remove("btn__submit--deactivated")
+  blockGroupLesson.classList.remove("price__block--active");
+  blockIndividualLesson.classList.add("price__block--active");
+})
